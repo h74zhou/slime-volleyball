222d23350e6194479ec81ea652b2f73b84052076
Henry Zhou <henry.zhou@shopify.com>
2021-07-24 11:30:30 -0400

    implemented collision handling

--- client/src/components/Game/Canvas.tsx
+++ client/src/components/Game/Canvas.tsx
@@ -25,27 +25,65 @@ const Canvas = props => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const firstPlayerRef = useRef({
-    w: 120,
-    h: 120,
-    x: 0,
-    y: 540,
+    w: 60,
+    h: 60,
+    x: 120,
+    y: 600,
     speed: 10,
     dx: 0,
     dy: 0,
   });
 
   const secondPlayerRef = useRef({
-    w: 120,
-    h: 120,
-    x: 400,
-    y: 540,
+    w: 60,
+    h: 60,
+    x: 880,
+    y: 600,
     speed: 10,
     dx: 0,
     dy: 0,
+  });
+
+  const volleyBallRef = useRef({
+    w: 20,
+    h: 20,
+    x: 20,
+    y: 300,
+    speed: 15,
+    dx: 5,
+    dy: 5,
   })
 
   const gravity = 0.2;
   const drag = 1;
+
+  const updateVolleyBallLocation = () => {
+    volleyBallRef.current.x += volleyBallRef.current.dx;
+
+    if (canvasRef.current != null && volleyBallRef.current.y < canvasRef.current?.height - volleyBallRef.current.h * 2) {
+      volleyBallRef.current.dy += gravity;
+    }
+    
+    volleyBallRef.current.y += volleyBallRef.current.dy;
+
+    if (canvasRef.current != null && 
+        (volleyBallRef.current.x < 0 + volleyBallRef.current.w || 
+          volleyBallRef.current.x > canvasRef.current?.width - volleyBallRef.current.w ||
+          ballPlayerCollision()
+        )
+      ) {
+      volleyBallRef.current.dx *= -1;
+    }
+
+    if (canvasRef.current != null && 
+        (volleyBallRef.current.y < 0 + volleyBallRef.current.w || 
+          volleyBallRef.current.y > canvasRef.current?.height - volleyBallRef.current.h ||
+          ballPlayerCollision()
+        )
+      ) {
+      volleyBallRef.current.dy *= -1;
+    }
+  };
 
   const updatePlayerLocation = (playerRef) => {
     playerRef.current.x += playerRef.current.dx;
@@ -53,8 +91,8 @@ const Canvas = props => {
     playerRef.current.dy *= drag;
     playerRef.current.y += playerRef.current.dy;
     
-    if (canvasRef.current != null && playerRef.current.x < 0) {
-      playerRef.current.x = 0;
+    if (canvasRef.current != null && playerRef.current.x < 0 + playerRef.current.w) {
+      playerRef.current.x = 0 + playerRef.current.w;
       playerRef.current.dx = 0;
     }
 
@@ -63,11 +101,23 @@ const Canvas = props => {
       playerRef.current.dx = 0;
     }
 
-    if (canvasRef.current != null && playerRef.current.y >= 540) {
-      playerRef.current.y = 540;
+    if (canvasRef.current != null && playerRef.current.y > canvasRef.current.height) {
+      playerRef.current.y = canvasRef.current.height;
       playerRef.current.dy = 0;
     }
   };
+
+  const ballPlayerCollision = () => {
+    const combinedRadiusFirst = firstPlayerRef.current.w + volleyBallRef.current.w;
+    const combinedRadiusSecond = secondPlayerRef.current.w + volleyBallRef.current.w;
+    const yDeltaFirst = Math.abs(volleyBallRef.current.y - firstPlayerRef.current.y);
+    const xDeltaFirst = Math.abs(volleyBallRef.current.x - firstPlayerRef.current.x);
+    const yDeltaSecond = Math.abs(volleyBallRef.current.y - secondPlayerRef.current.y);
+    const xDeltaSecond = Math.abs(volleyBallRef.current.x - secondPlayerRef.current.x);
+    const deltaDistanceFirst = Math.sqrt(Math.pow(yDeltaFirst, 2) + Math.pow(xDeltaFirst, 2)); 
+    const deltaDistanceSecond = Math.sqrt(Math.pow(yDeltaSecond, 2) + Math.pow(xDeltaSecond, 2)); 
+    return combinedRadiusFirst >= deltaDistanceFirst || combinedRadiusSecond >= deltaDistanceSecond;
+  }
 
   const updateFirstPlayerLocation = () => {
     updatePlayerLocation(firstPlayerRef);
@@ -78,9 +128,8 @@ const Canvas = props => {
   }
 
   const drawFirstPlayer = ctx => {
-    
     ctx.beginPath();
-    ctx.arc(firstPlayerRef.current.x + 60, firstPlayerRef.current.y + 60, 60, 0, Math.PI, true);
+    ctx.arc(firstPlayerRef.current.x, firstPlayerRef.current.y, firstPlayerRef.current.w, 0, Math.PI, true);
     ctx.closePath();
     ctx.fillStyle = '#000000';
     ctx.fill();
@@ -88,11 +137,18 @@ const Canvas = props => {
 
   const drawSecondPlayer = ctx => {
     ctx.beginPath();
-    ctx.arc(secondPlayerRef.current.x + 60, secondPlayerRef.current.y + 60, 60, 0, Math.PI, true);
+    ctx.arc(secondPlayerRef.current.x, secondPlayerRef.current.y, secondPlayerRef.current.w, 0, Math.PI, true);
     ctx.closePath();
-    ctx.fillStyle = '#800000';
+    ctx.fillStyle = '#800040';
     ctx.fill();
-  }
+  };
+
+  const drawVolleyBall = ctx => {
+    ctx.beginPath();
+    ctx.arc(volleyBallRef.current.x, volleyBallRef.current.y, volleyBallRef.current.w, 0, Math.PI * 2, true)
+    ctx.fillStyle = '#F5C600';
+    ctx.fill()
+  };
 
   const clear = ctx => {
    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -136,12 +192,10 @@ const Canvas = props => {
 
   useEffect(() => {
     if (keyUp) {
-      console.log(`Key Up Called, Name is: ${name}`)
-      console.log(`Key Up Called, First Player is: ${firstPlayer}`);
-      console.log(`Key Up Called, Second Player is: ${secondPlayer}`);
+      console.log("KEY UP was hit");
       if (
-        (name === firstPlayer && firstPlayerRef.current.y === 540) ||
-        (name === secondPlayer && secondPlayerRef.current.y === 540)
+        (name === firstPlayer && firstPlayerRef.current.y === canvasRef.current?.height) ||
+        (name === secondPlayer && secondPlayerRef.current.y === canvasRef.current?.height)
       ) {
         console.log("sending arrow up");
         sendMove("ArrowUp")
@@ -170,8 +224,10 @@ const Canvas = props => {
           clear(ctx);
           updateFirstPlayerLocation();
           updateSecondPlayerLocation();
+          updateVolleyBallLocation();
           drawFirstPlayer(ctx);
           drawSecondPlayer(ctx);
+          drawVolleyBall(ctx);
           requestAnimationFrame(render);
         }
         render();
